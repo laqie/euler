557859e3a6505f3cbd6546ec45c3522340690871
Alexander Popelnukhin <mail@laqie.net>
2013-08-21 12:35:21 +0700

    Task 47 (first try)

--- task047.py
+++ task047.py
@@ -1,28 +1,16 @@
-from primes import primes
+from factors import factor
+from time import time
 
-# all_primes = primes(9999)
-
-def factors(n):
-    p = primes(n + 1)
-    n = float(n)
-    result = []
-    while n > 1:
-        for i in p:
-            if n % i == 0:
-                result.append(i)
-                n /= i
-    return set(result)
-
-n, l, k = 61000, 4, 0
+s = time()
+n, l, k = 2, 4, 0
 while True:
-    if len(factors(n)) == l:
+    if len(set(factor(n))) == l:
         k += 1
-        # print n, k, factors(n)
     else:
         k = 0
     if k == l:
         print n - k + 1
         break
-    elif k >= 2:
-        print n, factors(n)
     n += 1
+
+print 'Elapsed time:', time() - s
